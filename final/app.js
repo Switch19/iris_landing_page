30e6eca7d585c40e70fad16c04d5a068449c124a
SwitchTega <eokoloho@gmail.com>
2024-04-14 13:00:57 +0100

    added the menu toggle function

--- final/app.js
+++ final/app.js
@@ -15,11 +15,8 @@ closeBtn.addEventListener("click", function () {
   sidebar.classList.remove("show-sidebar");
 });
 
-
 let get_date = document.querySelector('.date')
 const new_date = new Date().getFullYear()
 get_date.innerHTML = `${new_date}`
-// console.log(get_date.innerHTML = `${new_date}`
-// )
-
- 
+console.log(get_date.innerHTML = `${new_date}`
+)
